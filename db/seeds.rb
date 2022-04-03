03a3cdbdbe309a643d788477a48f1b263533a7bf
Slavahaim <slava.irizanov@gmail.com>
2022-04-03 23:57:09 +0300

    Added Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |i|
+  #Merchant.create(name: Faker::Company.name , city: Faker::Address.city , street: Faker::Address.street_address , country_code: Faker::Address.zip )
+  Merchant.create(name: "Company ##{i}", city: "City_#{i}", street: "street_#{i}", country_code: "#{i}12345#{i}")
+end
